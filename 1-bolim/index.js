477b81f44ac4ee35f5339b320368111d822fac3d
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2025-03-05 10:49:29 +0500

    1000masala  yechimi 12-14

---
+++ 1-bolim/index.js
@@ -0,0 +1,72 @@
+//shart operatorlari 10dan boshlab
+
+//10
+// let a=5
+// let b = 5
+//  let res;
+// if (a == b) {
+//  res=0
+// } else {
+//     res=a+b
+// }
+// console.log(res);
+
+// 11-masala
+// let a = prompt("a sonni kiriting");
+// let b = prompt("b sonni kiriting");
+// let res;
+// if (a == b) {
+//   res = 0;
+// } else if (a > b) {
+//   res = a;
+// } else {
+//   res = b;
+// }
+// console.log( res);
+
+//12-masala
+// let a = prompt("1 sonni kiriting ");
+// let b = prompt("2 sonni kriting");
+// let c = prompt(" 3-sonni kiriting");
+// let res;
+// if (a > c && b > c) {
+//   res = c;
+// } else if (a < b && a < c) {
+//   res = a;
+// } else if (a > b && b < c) {
+//   res = b;
+// }
+// console.log(res);
+
+// 13-masala
+// let a = Number(prompt("1 sonni kiriting"));
+// let b = Number(prompt("2 sonni kiriting"));
+// let c = Number(prompt("3 sonni kiriting"));
+// let res;
+// if ((a < b && a > c) || (a > b && a < c)) {
+//   res = a;
+// } else if ((a > b && c < b) || (a < b && b < c)) {
+//   res = b;
+// } else if ((c < b && c > a) || (c > b && c < a)) {
+//   res = c;
+// }
+// console.log(res);
+
+// 14-maasala
+// let a = Number(prompt("1 sonni kiriting"));
+// let b = Number(prompt("2 sonni kiriting"));
+// let c = Number(prompt("3 sonni kiriting"));
+// let res;
+// let reskatta;
+// if ((a < b && a < c && b < c) || (a < b && a < c && b > c)) {
+//   res = a;
+//   reskatta = c;
+// } else if ((b < a && b < c && a > c) || (b < a && b < c && a < c)) {
+//   res = b;
+//   reskatta = a;
+// } else if ((c < a && c < b && a < b) || (c < a && c < b && a > b)) {
+//   res = c;
+//   reskatta = b;
+// }
+// console.log(res);
+// console.log(reskatta);
