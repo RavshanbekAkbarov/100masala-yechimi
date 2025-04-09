276ec4661e25571009d2ba7f4f09344cc7f2ea52
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2025-04-09 20:05:55 +0500

    20-21 masalalr Abramovdan

--- 1-bolim/index.js
+++ 1-bolim/index.js
@@ -130,37 +130,78 @@
 // console.log(res);
 
 // 19-masala
-let son1 = prompt("1-sonni kirting");
-let son2 = prompt("2-sonni kriting");
-let son3 = prompt("3-sonni kiriting");
-let son4 = prompt("4-sonni kiriting");
-let res;
-if (son1 == son2 && son2 == son3) {
-  res = "4-raqam";
-} else if (son1 == son2 && son2 == son4) {
-  res = "3-raqam";
-} else if (son2 == son3 && son3 == son4) {
-  res = "1-raqam";
-} else if (son1 == son3 && son3 == son4) {
-  res = "2-raqam";
-}
-console.log(res);
+// let son1 = prompt("1-sonni kirting");
+// let son2 = prompt("2-sonni kriting");
+// let son3 = prompt("3-sonni kiriting");
+// let son4 = prompt("4-sonni kiriting");
+// let res;
+// if (son1 == son2 && son2 == son3) {
+//   res = "4-raqam";
+// } else if (son1 == son2 && son2 == son4) {
+//   res = "3-raqam";
+// } else if (son2 == son3 && son3 == son4) {
+//   res = "1-raqam";
+// } else if (son1 == son3 && son3 == son4) {
+//   res = "2-raqam";
+// }
+// console.log(res);
 
-// let son1 = Number(prompt("1-sonni kirting"));
-// let son2 = Number(prompt("2-sonni kriting"));
-// let son3 = Number(prompt("3-sonni kiriting"));
-// let son4 = Number(prompt("4-sonni kiriting"));
+// // let son1 = Number(prompt("1-sonni kirting"));
+// // let son2 = Number(prompt("2-sonni kriting"));
+// // let son3 = Number(prompt("3-sonni kiriting"));
+// // let son4 = Number(prompt("4-sonni kiriting"));
 
-let sonlar = [son1, son2, son3, son4];
+// let sonlar = [son1, son2, son3, son4];
 
-let nechtadanligi = {};
-sonlar.forEach((son) => (nechtadanligi[son] = (nechtadanligi[son] || 0) + 1));
+// let nechtadanligi = {};
+// sonlar.forEach((son) => (nechtadanligi[son] = (nechtadanligi[son] || 0) + 1));
+
+// // let res;
+
+// sonlar.forEach((son, index) => {
+//   if (nechtadanligi[son] == 1) {
+//     res = `${i + 1}-raqam`;
+//   }
+// });
+// console.log(res);
+
+// 20-masala
+// A nuqtasi
+// let ax = +prompt("A nuqtaning x koordinatasini kiriting:");
+// let ay = +prompt("A nuqtaning y koordinatasini kiriting:");
+
+// // B nuqtasi
+// let bx = +prompt("B nuqtaning x koordinatasini kiriting:");
+// let by = +prompt("B nuqtaning y koordinatasini kiriting:");
+
+// // C nuqtasi
+// let cx = +prompt("C nuqtaning x koordinatasini kiriting:");
+// let cy = +prompt("C nuqtaning y koordinatasini kiriting:");
+
+// const AdanBgacha = Math.sqrt((bx - ax) ** 2 + (by - ay) ** 2);
+// const AdanCgacha = Math.sqrt((cx - ax) ** 2 + (cy - ay) ** 2);
 
 // let res;
 
-sonlar.forEach((son, index) => {
-  if (nechtadanligi[son] == 1) {
-    res = `${i + 1}-raqam`;
-  }
-});
+// if (AdanBgacha < AdanCgacha) {
+//   res = `A nuqtasiga eng yaqin nuqta B. Masofa: ${AdanBgacha}`;
+// } else {
+//   res = `A nuqtasiga eng yaqin nuqta C. Masofa: ${AdanCgacha}`;
+// }
+
+// console.log(res);
+
+let x = +prompt("x o'qidagi qiymatni kiriting");
+let y = +prompt("y o'qidagi qiymatni kiriting");
+
+let res;
+if(x == 0 && y == 0)  {
+  res = 0;
+} else if (y === 0) {
+  res = 1;
+} else if (x === 0) {
+  res = 2;
+} else {
+  res = 3;
+}
 console.log(res);
